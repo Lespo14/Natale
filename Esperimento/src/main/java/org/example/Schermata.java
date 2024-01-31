0347f12048d81ec312554ad2ed5ce3c17ef9be79
Lespo14 <lollomichi@gmail.com>
2024-01-31 09:05:56 +0100

    Crezione base frame

---
+++ Esperimento/src/main/java/org/example/Schermata.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
+
+public  class Schermata extends Mioframe implements ActionListener, WindowListener {
+    public Schermata() {
+    }
+
+    public Schermata(String titolo) {
+        super(titolo);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
