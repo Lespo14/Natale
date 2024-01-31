5c4fe6d03f4ceecfb59cca608fb19ff045210028
Lespo14 <lollomichi@gmail.com>
2024-01-31 09:55:27 +0100

    Crezione base frame

--- Esperimento/src/main/java/org/example/Schermata.java
+++ Esperimento/src/main/java/org/example/Schermata.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowListener;
@@ -10,6 +12,30 @@ public  class Schermata extends Mioframe implements ActionListener, WindowListen
 
     public Schermata(String titolo) {
         super(titolo);
+        GridLayout gl = new GridLayout(3,2,50,30);
+        this.setLayout(gl);
+
+        JLabel casa = new JLabel("Home");
+        casa.setFont(new Font("MioFont",Font.ITALIC,14));
+        this.add(casa);
+
+        JLabel ospiti = new JLabel("Guest");
+        ospiti.setFont(new Font("MioFont",Font.ITALIC,14));
+        this.add(ospiti);
+
+        JLabel punti_Casa = new JLabel("0");
+        punti_Casa.setFont(new Font("MioFont",Font.BOLD,42));
+        this.add(punti_Casa);
+
+        JLabel punti_ospiti = new JLabel("0");
+        punti_ospiti.setFont(new Font("MioFont",Font.BOLD,42));
+        this.add(punti_ospiti);
+
+        JButton goal_Casa = new JButton("+");
+        this.add(goal_Casa);
+
+        JButton goal_Ospiti = new JButton("+");
+        this.add(goal_Ospiti);
     }
 
     @Override
